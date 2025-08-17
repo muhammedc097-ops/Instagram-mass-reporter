17c9a19923ffc6dc0b60853a2ed1797ebd029eee
muhammedc097-ops <muhammedc097@gmail.com>
2025-08-17 03:07:14 -0700

    nun

--- Instagram Report Bot2.py
+++ Instagram Report Bot2.py
@@ -9,7 +9,7 @@ import sys
 def getOptions(args=sys.argv[1:]):
 
     parser = argparse.ArgumentParser(description="This bot helps users to mass report accounts with clickbaits or objectionable material.")
-    parser.add_argument("-u", "--Davvclaudio", type = str, default = "", help = "Davvclaudio to report.")
+    parser.add_argument("-u", "--Davvclaudio", type = str, default = "", help to report.")
     parser.add_argument("-f", "--file", type = str, default = "acc.txt", help = "Accounts list ( Defaults to acc.txt in program directory ).")
 
     options = parser.parse_args(args)
